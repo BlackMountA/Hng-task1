ab3f54b68df45acc293ccde6b4232a7af0dcfc35
Adewoyin Aanuoluwapo <adewoyinaanuoluwapo@gmail.com>
2022-11-05 21:44:06 +0100

    fix: Bug fixed hopefully

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -1,32 +1,64 @@
-import React from 'react'
-
+import { useForm } from "react-hook-form"
 const ContactForm = () => {
-    const name = 'Aanuoluwapo'
+    const name = 'Aanuoluwapo';
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    }
     return (
-      <form>
-      <div>
-          <div className='input-field' id='first_name'>
-              <label> First name</label>
-              <input type={'text'} />
-          </div>
-          <div className='input-field' id='last_name'>
-              <label> Last name</label>
-              <input type={'text'} />
-          </div>
-          <div className='input-field' id='email'>
-              <label> Email</label>
-              <input type={'text'} />
-          </div>
-          <div>
-                <label>Message</label>
-                <textarea id='message'></textarea>   
-          </div>
-          <div>
-                <input type={'checkbox'} />   
-                    <label>You agree to providing your data to {name} who may contact you</label>
-          </div>     
-        </div>
-            <button>Send Message</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='field'>
+                <div className='name-row'>
+                    <div className='input-field' >
+                        <label> First name</label>
+                        <input type={'text'} id='first_name' placeholder='Enter your first name'
+                            {...register('firstname', { required: true })} />
+                        <p className="error">
+                            {errors.firstname?.type === 'required' && 'First name is required'}
+                        </p>
+                    </div>
+
+                    <div className='input-field' >
+                        <label> Last name</label>
+                        <input type={'text'} id='last_name' placeholder='Enter your last name' 
+                            {...register('lastname', { required: true })} />
+                        <p className="error">
+                            {errors.lastname?.type === 'required' && 'Last name is required'}
+                        </p>
+                    </div>
+                </div>
+
+                <div className='email '>
+                    <label> Email</label>
+                    <input type={'text'} id="email" placeholder='yourname@email.com'{...register('email', { required: true, })}  />
+                    <p className="error">
+                        {errors.email?.type === 'required' && 'Email is required'}
+                    </p>
+                </div>
+
+                <div className='message'>
+                    <label>Message</label>
+                    <textarea id='message'  placeholder="Send me a message and I'll reply you as soon as possible"{...register('message', { required: "Please enter a message" })} />
+                    {errors.message && (<p className="error" >
+                        {errors.message?.type === 'required' && ('Please enter a message')}
+                    </p>)}
+                    
+                </div>
+
+                <div  className="checkbox">
+                    <div className='checkbox-container'>
+                        <input type={'checkbox'} {...register('checkbox', { required: true })}    />   
+                        <label>You agree to providing your data to {name} who may contact you</label>
+                    
+                    </div>
+                    <p className="error">
+                        {errors.checkbox?.type === 'required' && 'Tick the box'}
+                    </p>
+                </div>     
+            </div>
+
+            <button id='btn__submit'>Send Message</button>
         </form>
   )
 }
